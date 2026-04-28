eaeca4e21bf8e18f2fcd2ecf5befb1d70db1fce5
Abdullah <abdullahawad835@gmail.com>
2026-04-28 13:52:51 +0200

    apc cleaning func

---
+++ src/cleaning.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+def clean_apc(filepath):
+    apc = pd.read_csv(filepath, sep=";", encoding='latin-1')
+
+    columns_to_drop = [
+    'fileName',                   # Doesn't help
+    'stationName',                # always empty
+    'stationCode',                # always empty
+    'gpsDeviation',               # always '0 m'
+    'isGpsMapped',                # always False
+    'wheelchairRampAccessCount',  # always 0
+    'isBicycleRackAccessed',      # always False
+    'area',                       # always empty
+    'netstop_latitude',           # always empty
+    'netstop_longitude',          # always empty
+    ]
+
+    # Drop chosen columns in 'columns_to_drop'
+    apc_clean = apc.drop(columns= columns_to_drop)
+    # Drop duplicated rows
+    apc_clean = apc_clean.drop_duplicates().reset_index(drop=True)
+    # Drop empty rows
+    apc_clean = apc_clean.dropna(how='all').reset_index(drop=True)
+
+    print("before", apc.shape)
+    print("after", apc_clean.shape)
+
+    apc_clean.to_csv('../data/processed/apc_clean.csv', sep=';', index=False, encoding='latin-1')
+
+    print("Saved to ../data/processed/")
+
+    return apc_clean
+    
